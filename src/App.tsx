2c7f98370729123106bd28e3c50802db9801244a
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-09-16 19:16:47 +0530

    fix: updated display text

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function App() {
-  return <div className='p-5 text-red-400 bg-blue-600'>Hello Mars!</div>
+  return <div className='p-5 text-red-400 bg-blue-600'>Hello Venus!</div>
 }
 
 export default App
--- src/components/SampleComp/SampleComp.tsx
+++ src/components/SampleComp/SampleComp.tsx
@@ -2,7 +2,7 @@ export interface ISampleCompProps {}
 
 const SampleComp = () => (
   <div className='p-5 bg-red-400'>
-    🛬 Hello, This is SampleComp component! 🛫
+    Hello, This is SampleComp component! From venus!
   </div>
 )
 
