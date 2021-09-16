aeee2b0fdf57ca11470a6b476e0974e2a225fd5e
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-09-16 17:45:06 +0530

    fix: modified the absolute imports to relative imports

--- src/components/SampleComp/SampleComp.tsx
+++ src/components/SampleComp/SampleComp.tsx
@@ -1,11 +1,8 @@
-import SampleAtom from '@atoms/SampleAtom'
-
 export interface ISampleCompProps {}
 
 const SampleComp = () => (
   <div className='p-5 bg-red-400'>
     🛬 Hello, This is SampleComp component! 🛫
-    <SampleAtom />
   </div>
 )
 
