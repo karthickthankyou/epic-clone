dadf7950fa93d662b7de7cfad71d62a5d5dbd8c8
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-09-25 23:11:21 +0530

    testing lint staged 2

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-unused-vars */
 module.exports = {
   '*.{ts,tsx}': (filenames) => [
-    'npm run format:write',
-    'git add',
-    'npm run format:check',
-    'npm run lint',
-    'npm run type:check',
+    'npm run format:fix',
+    'npm run validate',
+    // 'npm run format:check',
+    // 'npm run lint',
+    // 'npm run type:check',
   ],
 }
