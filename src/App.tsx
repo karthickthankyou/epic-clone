948838d68a70ffc6e781891476d0782ee8ff796d
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-09-28 22:56:02 +0530

    fix: cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,5 @@
-import SampleComp from './components/SampleComp'
-import { useAppDispatch, useAppSelector } from './store/hooks'
-import { getUser, setUser } from './store/slices/UserSlice'
-
 function App() {
-  const dispatch = useAppDispatch()
-  const updateUser = () => {
-    dispatch(setUser({ uid: '123', displayName: 'Karthick' }))
-  }
-  const user = useAppSelector(getUser)
-  return (
-    <div className="p-5">
-      Hello World!
-      <br />
-      <br />
-      <button
-        type="button"
-        onClick={updateUser}
-        className="px-2 py-1 mb-2 border rounded-sm"
-      >
-        Set User
-      </button>
-      <br />
-      <SampleComp />
-      <div>
-        uid: <span>{user.uid}</span>
-        <br />
-        displayName: <span>{user.displayName}</span>
-      </div>
-    </div>
-  )
+  return <div className="p-5">Hello World!</div>
 }
 
 export default App
--- src/components/SampleComp/SampleComp.tsx
+++ src/components/SampleComp/SampleComp.tsx
@@ -1,26 +1,7 @@
-import { useAppDispatch } from '../../store/hooks'
-import { setUser } from '../../store/slices/UserSlice'
-
 export interface ISampleCompProps {}
 
-const SampleComp = () => {
-  const dispatch = useAppDispatch()
-  const updateUser = () => {
-    dispatch(setUser({ uid: '&!&n@', displayName: '&@OP@*&^* @&*^*&@' }))
-  }
-  return (
-    <div className="p-5 bg-red-400">
-      Hello, This is the polluting component!
-      <br />
-      <button
-        type="button"
-        onClick={updateUser}
-        className="px-2 py-1 mt-1 bg-white rounded-sm"
-      >
-        Pollute the store
-      </button>
-    </div>
-  )
-}
+const SampleComp = () => (
+  <div className="p-5 bg-red-400">Hello, This is the polluting component!</div>
+)
 
 export default SampleComp
