787e6dccd0a6c7455cc2d2b2d2052a7bf17d44be
Karthick Ragavendran <karthickthankyou@gmail.com>
2021-09-28 12:53:24 +0530

    fix: cleanup

--- craco.config.js
+++ craco.config.js
@@ -6,19 +6,4 @@ module.exports = {
       plugins: [require('tailwindcss'), require('autoprefixer')],
     },
   },
-  webpack: {
-    alias: {
-      '@components': path.resolve('src/components'),
-      '@atoms': path.resolve('src/components/atoms'),
-      '@molecules': path.resolve('src/components/molecules'),
-      '@organisms': path.resolve('src/components/organisms'),
-      '@pages': path.resolve('src/components/pages'),
-      '@hooks/index': path.resolve('src/hooks'),
-      '@utils': path.resolve('src/utils'),
-      '@store': path.resolve('src/store'),
-      '@assets': path.resolve('src/assets'),
-      '@epicfirebase': path.resolve('src/firebase'),
-      '@epictypes': path.resolve('src/types'),
-    },
-  },
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
-import React from 'react'
-import SampleAtom from '@atoms/SampleAtom'
-
-// import SampleComp from '@atoms/SampleComp'
-
 function App() {
-  return (
-    <div className="p-5 text-red-400 bg-blue-600">
-      Hello World!
-      <SampleAtom />
-    </div>
-  )
+  return <div className="p-5 text-red-400 bg-blue-600">Hello World!</div>
 }
 
 export default App
